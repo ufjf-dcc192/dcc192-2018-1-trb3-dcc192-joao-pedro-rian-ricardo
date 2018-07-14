4a20f1b0ed17706ce99fdd5418ac2453c623f888
Rian Alves <Rian Alves@RIAN.local>
2018-07-14 02:41:55 -0300

    >>Criação de UsuarioDAO > Alteração do RegistroPostCommand >> Atualização de dependencias no Pom

--- src/main/java/Comandos/RegistroPostCommand.java
+++ src/main/java/Comandos/RegistroPostCommand.java
@@ -1,7 +1,7 @@
 package Comandos;
 
-import DAOs.ParticipanteDAO;
-import Modelo.Participante;
+import DAOs.UsuarioDAO;
+import Models.Usuario;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -16,16 +16,14 @@ public class RegistroPostCommand implements Comando {
      @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
         String nome = request.getParameter("nome");
+        String login = request.getParameter("login");
         String email = request.getParameter("email");
         String senha = request.getParameter("senha");
-        ParticipanteDAO dao = ParticipanteDAO.getInstance();
-        Participante participante = new Participante(nome, email, senha);     
-        try {
-            dao.adicionar(participante);
-            response.sendRedirect("index.html");
-        } catch (SQLException | IOException ex) {
-            Logger.getLogger(NovoEventoPostCommand.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        UsuarioDAO dao = UsuarioDAO.getInstance();
+        Usuario usuario = new Usuario(nome,login, email, senha);     
+        dao.adicionar(usuario);
+        response.sendRedirect("login.html");
+       
     }
     
 }
---
+++ src/main/java/DAOs/UsuarioDAO.java
@@ -0,0 +1,20 @@
+
+package DAOs;
+
+import Models.Usuario;
+
+/**
+ *
+ * @author Rian Alves
+ */
+public class UsuarioDAO {
+
+    public static UsuarioDAO getInstance() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void adicionar(Usuario participante) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
