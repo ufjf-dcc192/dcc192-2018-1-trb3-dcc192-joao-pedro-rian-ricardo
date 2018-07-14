3442e1e7014d5585bf85b17e890a461612b56f4a
Joao Paulo <jpdias1997@hotmail.com>
2018-07-14 17:01:12 -0300

    GetInstancia do UsuarioDAO e Adicionar Usuário com senha MD5

--- src/main/java/DAOs/UsuarioDAO.java
+++ src/main/java/DAOs/UsuarioDAO.java
@@ -2,19 +2,46 @@
 package DAOs;
 
 import Models.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Rian Alves
  */
 public class UsuarioDAO {
+    
+    private static Connection conexao;
+    private static UsuarioDAO dao;
 
+    public UsuarioDAO() throws SQLException {
+        UsuarioDAO.conexao = Conexao.getConexao();
+    }
     public static UsuarioDAO getInstance() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       if (dao == null) {
+            try {
+                dao = new UsuarioDAO();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return dao;
     }
 
-    public void adicionar(Usuario participante) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void adicionar(Usuario usuario) {
+         String sql = "INSERT INTO USUARIO(nome, email,senha) VALUES(?,?,MD5(?))";
+        try (PreparedStatement comando = conexao.prepareStatement(sql)) {
+            comando.setString(1, usuario.getNome());
+            comando.setString(2, usuario.getEmail());
+            comando.setString(3, usuario.getSenha());
+            comando.execute();
+            comando.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
