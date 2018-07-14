b3a5538a5c94a0c2567e662683ddacfac9e0d891
Ricardo Alves <Ricardo Alves@DESKTOP-T6FOIFI>
2018-07-13 21:46:55 -0300

    Criação da model item

---
+++ src/main/java/Models/Item.java
@@ -0,0 +1,65 @@
+package Models;
+
+import java.util.Date;
+
+public class Item {
+    private Integer id;
+    private String titulo;
+    private String descricao;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private Usuario usuario;
+
+    public Item() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+}
