b3cdfdffd46d7b8cc8b026b1c6925f95774f6f78
Ricardo Alves <Ricardo Alves@DESKTOP-T6FOIFI>
2018-07-13 21:42:00 -0300

    Criação da model usuario

---
+++ src/main/java/Models/Usuario.java
@@ -0,0 +1,62 @@
+package Models;
+
+public class Usuario {
+
+private Integer id;
+private String nome;
+private String email;
+private String login;
+private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email, String login, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+}
