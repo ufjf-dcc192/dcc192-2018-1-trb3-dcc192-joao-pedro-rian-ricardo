2eacb011578db0a310a7ae66827e4e693e850748
Rian Alves <Rian Alves@RIAN.local>
2018-07-15 20:33:59 -0300

    > Criação do novoItem.jsp > Exclusão do método listById no usuário DAO >ItemNovoCommand

--- src/main/java/Comandos/ItemNovoCommand.java
+++ src/main/java/Comandos/ItemNovoCommand.java
@@ -17,17 +17,22 @@ public class ItemNovoCommand implements Comando {
 
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
-        
+
         try {
             response.setContentType("text/html;charset=UTF-8");
             Integer idUsuario = (Integer) request.getSession().getAttribute("usuario");
+            UsuarioDAO dao = UsuarioDAO.getInstance();
+            Usuario usuario = dao.listbyID(idUsuario);
+            request.setAttribute("usuario", usuario);
             RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/novoItem.jsp");
             despachante.forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(ItemNovoCommand.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ServletException ex) {
             Logger.getLogger(ItemNovoCommand.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(ItemNovoCommand.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
 
+    }
 }
