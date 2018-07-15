165cd75dce2607c6665a5ba06b7a8205771959c7
Joao Paulo <jpdias1997@hotmail.com>
2018-07-15 10:32:36 -0300

    Adição do select para o Login com MD5

--- src/main/java/DAOs/UsuarioDAO.java
+++ src/main/java/DAOs/UsuarioDAO.java
@@ -1,9 +1,9 @@
-
 package DAOs;
 
 import Models.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -13,18 +13,19 @@ import java.util.logging.Logger;
  * @author Rian Alves
  */
 public class UsuarioDAO {
-    
+
     private static Connection conexao;
     private static UsuarioDAO dao;
 
-    public UsuarioDAO() throws SQLException {
+    public UsuarioDAO() throws SQLException, ClassNotFoundException {
         UsuarioDAO.conexao = Conexao.getConexao();
     }
+
     public static UsuarioDAO getInstance() {
-       if (dao == null) {
+        if (dao == null) {
             try {
                 dao = new UsuarioDAO();
-            } catch (SQLException ex) {
+            } catch (SQLException | ClassNotFoundException ex) {
                 Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
@@ -32,7 +33,7 @@ public class UsuarioDAO {
     }
 
     public void adicionar(Usuario usuario) {
-         String sql = "INSERT INTO USUARIO(nome, email,senha) VALUES(?,?,MD5(?))";
+        String sql = "INSERT INTO USUARIO(nome, email,senha) VALUES(?,?,MD5(?))";
         try (PreparedStatement comando = conexao.prepareStatement(sql)) {
             comando.setString(1, usuario.getNome());
             comando.setString(2, usuario.getEmail());
@@ -43,5 +44,20 @@ public class UsuarioDAO {
             Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    public Usuario getUsuario(String login, String senha) {
+        String sql = "SELECT ID FROM USUARIO WHERE LOGIN = ? AND SENHA = MD5(?)";
+        Usuario usuario = null;
+        try (PreparedStatement comando = conexao.prepareStatement(sql)) {
+            comando.setString(1, login);
+            comando.setString(2, senha);
+            ResultSet resultado = comando.executeQuery();
+            usuario = new Usuario(resultado.getInt("id"), resultado.getString("nome"), resultado.getString("email"), resultado.getString("login"));
+            comando.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return usuario;
+    }
+
 }
