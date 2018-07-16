e65d0ef2fc6751e9ce249804e4ed9d4777a4ced8
Pedro Freitas <soufreitas.pedro@gmail.com>
2018-07-16 00:55:24 -0300

    Criando o RankingCommand

---
+++ src/main/java/Comandos/RankigCommand.java
@@ -0,0 +1,31 @@
+package Comandos;
+
+import DAOs.ItemDAO;
+import Models.Item;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pedro
+ */
+public class RankigCommand implements Comando {
+
+    @Override
+    public void exec(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            List<Item> itens = ItemDAO.getInstance().getItensOrdenados();
+            request.setAttribute("itens", itens);
+            RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/ranking.jsp");
+            despachante.forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(RankigCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
