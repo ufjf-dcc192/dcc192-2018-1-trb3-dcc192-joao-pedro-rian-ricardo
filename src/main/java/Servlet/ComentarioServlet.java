aa9c06e6e8c7d249a9dbca9def9919ed1b569a6e
Joao Paulo <jpdias1997@hotmail.com>
2018-07-16 12:46:13 -0300

    Correção do  Ranking PostCommand

--- src/main/java/Servlet/ComentarioServlet.java
+++ src/main/java/Servlet/ComentarioServlet.java
@@ -54,7 +54,7 @@ public class ComentarioServlet extends HttpServlet {
             Map<String, String> rotas;
             rotas = new HashMap<>();
             rotas.put("/comentar.html", "Comandos.ComentarCommand");
-            rotas.put("/ranking.html", "Comandos.RankingCommand");
+            rotas.put("/ranking.html", "Comandos.RankingPostCommand");
             String clazzName = rotas.get(request.getServletPath());
 
             Comando comando;
