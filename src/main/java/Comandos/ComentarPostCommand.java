4558aa26ea916a1622f429b87607e83c6ea1c204
Pedro Freitas <soufreitas.pedro@gmail.com>
2018-07-15 23:13:23 -0300

    Consertando erro no postCommando do comentar

--- src/main/java/Comandos/ComentarPostCommand.java
+++ src/main/java/Comandos/ComentarPostCommand.java
@@ -11,6 +11,11 @@ import DAOs.UsuarioDAO;
 import Models.Comentario;
 import Models.Item;
 import Models.Usuario;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -22,18 +27,23 @@ public class ComentarPostCommand implements Comando {
 
     @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
-        String descricao = request.getParameter("descricao");
-        Integer idUsuario = (Integer) request.getSession().getAttribute("usuarioID");
-        UsuarioDAO dao = UsuarioDAO.getInstance();
-        Usuario usuario = dao.getUsuarioById(idUsuario);
-        Integer idItem = Integer.parseInt(request.getParameter("idItem"));
-        ItemDAO daoItem = ItemDAO.getInstance();
-        Item item = daoItem.getItemById(idItem);
-        ComentarioDAO daoC = ComentarioDAO.getInstance();
-        Comentario comentario = new Comentario(descricao, usuario, item);
-        daoC.adicionarComentario(comentario);
-        
-
+        try {
+            String descricao = request.getParameter("descricao");
+            Integer idUsuario = (Integer) request.getSession().getAttribute("usuarioID");
+            UsuarioDAO dao = UsuarioDAO.getInstance();
+            Usuario usuario = dao.getUsuarioById(idUsuario);
+            Integer idItem = Integer.parseInt(request.getParameter("idItem"));
+            ItemDAO daoItem = ItemDAO.getInstance();
+            Item item = daoItem.getItemById(idItem);
+            ComentarioDAO daoC = ComentarioDAO.getInstance();
+            Comentario comentario = new Comentario(descricao, usuario, item);
+            daoC.adicionarComentario(comentario);
+            request.setAttribute("enviado", true);
+            RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/comentar.jsp");
+            despachante.forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(ComentarPostCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
