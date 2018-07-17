cc2e0b5c5672f2af6c49c5a3a9a34661a72d4c42
Pedro Freitas <soufreitas.pedro@gmail.com>
2018-07-17 04:11:27 -0300

    Acrescentando o despachante e consertando erro no construtor

--- src/main/java/Comandos/RegistroPostCommand.java
+++ src/main/java/Comandos/RegistroPostCommand.java
@@ -5,27 +5,29 @@ import Models.Usuario;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class RegistroPostCommand implements Comando {
 
-     @Override
+    @Override
     public void exec(HttpServletRequest request, HttpServletResponse response) {
-        String nome = request.getParameter("nome");
-        String email = request.getParameter("email");
-        String login = request.getParameter("login");
-        String senha = request.getParameter("senha");
-        request.setAttribute("cadastro", true);
-        UsuarioDAO dao = UsuarioDAO.getInstance();
-        Usuario usuario = new Usuario(nome,login, email, senha);     
-        dao.adicionar(usuario);
-         try {
-             response.sendRedirect("index.html");
-         } catch (IOException ex) {
-             Logger.getLogger(RegistroPostCommand.class.getName()).log(Level.SEVERE, null, ex);
-         }
-       
+        try {
+            String nome = request.getParameter("nome");
+            String email = request.getParameter("email");
+            String login = request.getParameter("login");
+            String senha = request.getParameter("senha");
+            request.setAttribute("cadastro", true);
+            UsuarioDAO dao = UsuarioDAO.getInstance();
+            Usuario usuario = new Usuario(nome, email, login, senha);
+            dao.adicionar(usuario);
+            RequestDispatcher despachante = request.getRequestDispatcher("/WEB-INF/login.jsp");
+            despachante.forward(request, response);
+        } catch (ServletException | IOException ex) {
+            Logger.getLogger(RegistroPostCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
